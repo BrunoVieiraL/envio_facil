b0e541cc180885e4db861b75d66d961b2b41455b
Bruno Vieira <bruno.leo516@hotmail.com>
2022-09-26 15:41:23 -0300

    responsive UI on trip_created_page

--- lib/views/trip_created_page.dart
+++ lib/views/trip_created_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jera_app/constants.dart';
 import 'package:jera_app/widgets/custom_widget_bottom_green_button.dart';
 
@@ -10,40 +11,40 @@ class TripCreatedPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          const Padding(
+          Padding(
             padding: EdgeInsets.only(
-              top: 126,
-              bottom: 32,
-              left: 116,
-              right: 116,
+              top: 126.h,
+              bottom: 32.h,
+              left: 116.w,
+              right: 116.w,
             ),
             child: SizedBox(
-              height: 127,
-              width: 128,
-              child: Image(
+              height: 127.h,
+              width: 128.w,
+              child: const Image(
                 image: AssetImage('assets/paper_plane/paper_plane_send.png'),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              bottom: 17,
-              left: 75,
-              right: 75,
+            padding: EdgeInsets.only(
+              bottom: 17.h,
+              left: 75.w,
+              right: 75.w,
             ),
             child: Text(
               'Viagem criada',
               style: TextStyle(
-                  fontSize: 32,
+                  fontSize: 32.sp,
                   fontFamily: titilliumWebRegular,
                   fontWeight: FontWeight.w400),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              bottom: 29,
-              left: 24,
-              right: 24,
+            padding: EdgeInsets.only(
+              bottom: 29.h,
+              left: 24.w,
+              right: 24.w,
             ),
             child: Text(
               'Vamos ver os volumes já disponíveis para a sua viagem.',
@@ -53,18 +54,18 @@ class TripCreatedPage extends StatelessWidget {
               ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w),
             child: Text(
               'Ao prosseguir, você declara para efeitos legais, administrativos, jurídicos e demais aplicáveis, a veracidade de todas as informações prestadas antes, durante e após qualquer uma das etapas do app.',
               textAlign: TextAlign.center,
               style: TextStyle(
-                color: Color(0x2222228A),
-                fontSize: 12,
+                color: const Color(0x2222228A),
+                fontSize: 12.sp,
               ),
             ),
           ),
-          const SizedBox(height: 95),
+          SizedBox(height: 95.h),
           CustomWidgetBottomGreenButton(
               onPressed: () {
                 Navigator.of(context).pushReplacementNamed('/homePage');
